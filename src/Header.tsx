f1fe606f3dd8fb6f18c3a3f1d1234650a1eb60ec
Rajeev Singla <rajeev1.singla@gmail.com>
2024-09-23 01:54:42 -0700

    Updated Header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,36 +1,37 @@
 import React from 'react'
 
 import xoraLogo from '/images/xora.svg'
-import { Link } from 'react-scroll'
+import { Link as LinkScroll } from "react-scroll";
 
 
 const NavLink = ({title}: {title: string}) => (
-  <Link to="/" smooth={true} duration={500}>
+  <LinkScroll to={title} smooth={true} duration={500}>
     {title}
-  </Link>
+  </LinkScroll>
 )
 
 const Header = () => {
   return (
-    <header className='fixed top-0 left-0 w-full py-10'> 
+    <header className='fixed top-0 left-0 z-50 w-full py-10'> 
     <div className='flex container h-14 items-center max-lg:px-5'>
         <a className='lg:hidden flex-1 cursor-pointer z-2'>
-          <img src={xoraLogo} width={194} height={55} alt="Xora logo" />
+          <img src={xoraLogo} width={110} height={55} alt="Xora logo" />
         </a>
       </div>
-      <div className='w-full border-2  border-amber-400'>
+      <div className='w-full max-lg:hidden border-2  border-amber-400'>
         <nav>
-          <ul className='flex max-lg:block max-lg:px-12'>
+          <ul className='flex  max-lg:px-12'>
             <li className='nav-li'>
               <NavLink title="features"/>
-            </li>
-            <li className='nav-li'>
+              <div className='dot'/>
               <NavLink title="pricing"/>
+            </li>
+            <li className='nav-logo'>
+              <img src={xoraLogo} width={100} height={55} alt="Xora logo" />
             </li>
             <li className='nav-li'>
               <NavLink title="resources"/>
-            </li>
-            <li className='nav-li'>
+              <div className='dot'/>
               <NavLink title="company"/>
             </li>
           </ul>
