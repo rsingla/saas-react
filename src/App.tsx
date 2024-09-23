a3bc52b96a8e41a382cb98a8885eb003307f1ad4
Rajeev Singla <rajeev1.singla@gmail.com>
2024-09-23 00:34:49 -0700

    Committing the Saas-React project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Header from './header'
+import Card from './Card'
+
+function App() {
+
+  return (
+    <div className="App">
+       <Header />
+
+      <h1 className="text-3xl font-bold underline">
+        Hello World!
+      </h1>
+
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const Header = () => {
+  return (
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+  )
+}
+
+export default Header
